3d9625ff6f4915597b5c3549e83c2433b983ad1b
Samuel Cadiz <cadizs@moravian.edu>
2024-04-15 17:39:02 -0400

    add simple program and tests

---
+++ Arithmetic.py
@@ -0,0 +1,12 @@
+def Addition(a,b,c):
+    return a+b+c
+
+def Subtraction(a,b,c):
+    return a-b-c
+
+if __name__ == "__main__":
+    print("This is Arithmetic Module")
+    a = 1
+    b = 2
+    c = 3
+    addition = Addition(a, b, c)
